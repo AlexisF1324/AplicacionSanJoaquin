ad3fbbed2f5b7237d9194ea7c991934bdde0055d
unknown <Santo Tomas@SCL076PCL8001.alumnos.cst>
2023-08-23 12:22:57 -0400

    SubidaPrueba

---
+++ app/src/main/java/com/example/aplicacionsanjoaquin/Acceder.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionsanjoaquin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Acceder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
